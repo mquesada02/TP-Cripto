47f52cb8547142dc92078b0253f66936e6b7ba8d
Uriel Arias <uarias@itba.edu.ar>
2025-05-26 19:10:53 -0300

    feat: adding ImageParsing

--- src/main/java/ar/edu/itba/cys/Main.java
+++ src/main/java/ar/edu/itba/cys/Main.java
@@ -1,9 +1,14 @@
 package ar.edu.itba.cys;
 
+import ar.edu.itba.cys.system.ImageParsing;
 import ar.edu.itba.cys.system.Properties;
+import java.io.File;
 
 public class Main {
     public static void main(String[] arg) {
         Properties properties = Properties.getProperties();
+        File secretFile= properties.secret();
+        int[] imageSize = ImageParsing.getImageSizeForBMP(secretFile.getPath());
+        int[][] secretImageBitMap = ImageParsing.getGrayscaleImageUsingBMP(secretFile.getPath(), imageSize[0], imageSize[1]);
     }
 }
